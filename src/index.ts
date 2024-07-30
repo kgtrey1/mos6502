adbd30b17d98c6b2ef73de8052f10679fd79fc22
Kevin Gouyet <kevin.gouyet@protonmail.com>
2024-07-30 18:45:18 +0800

    add: content for npm release

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import mos6502 from "./mos6502";
 import { type AddressingModes, type AddressingModesMap } from "./addressing";
 import { type Instruction, type Instructions, type InstructionsMap } from "./instructions";
 import decode from "./decoder";
+import { hex, formatDisassembly, formatRegisters } from './formatter'
 
 export {
     type AddressingModes,
@@ -10,6 +11,9 @@ export {
     type Instructions,
     type InstructionsMap,
     decode,
+    hex,
+    formatDisassembly,
+    formatRegisters,
 }
 
 export default mos6502
